a8e711359869bf3951248351ce8afe4945aff779
mattiacallara <mattiacallara0@gmail.com>
2026-06-17 17:26:06 +0200

    add road bikes dataset

---
+++ snack1/main.js
@@ -0,0 +1,9 @@
+const biciDaCorsa = [
+    {nome: 'Cannondale Synapse Carbon 4', pesoKg: 9 },
+    {nome: 'BMC Roadmachine Two', pesoKg: 8.8},
+    {nome: 'Ribble Allroad SLR', pesoKg: 8.75},
+    {nome: 'Focus Paralane 8.9', pesoKg: 8.6},
+    {nome: 'Giant Defy Advanced 0', pesoKg: 8.32},
+    {nome: 'Canyon Endurace CF SLX 7 Di2', pesoKg: 8.18},
+    {nome: 'Cube Agree C62 Pro', pesoKg: 7.9},
+];
