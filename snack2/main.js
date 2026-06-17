aabb460c0f65feb9a238508299e4a8c138026fc4
mattiacallara <mattiacallara0@gmail.com>
2026-06-17 17:29:38 +0200

    add squadreDiCalcio array

---
+++ snack2/main.js
@@ -0,0 +1,8 @@
+const squadreDiCalcio = [
+    {nome:'Inter', punti: 0, falliSubiti: 0},
+    {nome:'Roma', punti: 0, falliSubiti: 0},
+    {nome:'Juve', punti: 0, falliSubiti: 0},
+    {nome:'Milan', punti: 0, falliSubiti: 0},
+    {nome:'Como', punti: 0, falliSubiti: 0},
+    {nome:'Napoli', punti: 0, falliSubiti: 0},
+];
